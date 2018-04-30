37848b84e8e5d0d4e031111f0161e0534e1db496
Max Krugovykh <krugovy@mail.ru>
2018-04-30 19:44:09 +0300

    Initial commit

---
+++ src/com/company/Players.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Players {
+String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/com/company/Ships.java
@@ -0,0 +1,29 @@
+package com.company;
+public class Ships {
+   int sizeShips;
+   int typeShips;
+    static final int SIZE_ONE_TYPE = 1;
+    static final int SIZE_TWO_TYPE = 2;
+    static final int SIZE_THREE_TYPE = 3;
+    static final int SIZE_FOR_TYPE = 4;
+    static final int ONE_TYPE = 4;
+    static final int TWO_TYPE = 3;
+    static final int THREE_TYPE = 2;
+    static final int FOR_TYPE = 1;
+    static final int ALL_TYPE = 4;
+    static final int ALL_SHIPS = ONE_TYPE + THREE_TYPE + TWO_TYPE + FOR_TYPE;
+
+
+
+
+
+
+    Ships(){
+       System.out.println("Ship Create!");
+   }
+
+
+
+
+
+}
